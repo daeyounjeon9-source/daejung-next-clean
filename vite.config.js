632caaa7a05cd88de9a327bbf3201cfb4a611b54
daeyounjeon9 <daeyounjeon9@gmail.com>
2026-07-05 23:04:52 +0900

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  root: '.'   // 👈 이거 추가
+  plugins: [react()]
 })
